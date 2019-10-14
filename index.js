13e4160f5285997a3d08682a2f3bc4c5641ff1a7
Maurício Baú <mauricio.a.bau@gmail.com>
2019-10-14 17:26:59 -0300

    Finalizado CRUD

--- index.js
+++ index.js
@@ -35,4 +35,21 @@ server.post('/users', (req, res) => {
   return res.json(users);
 });
 
+server.put('/users/:index', (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  users[index] = name;
+
+  return res.json(users);
+});
+
+server.delete('/users/:index', (req, res) => {
+  const { index } = req.params;
+
+  users.splice(index, 1);
+
+  return res.send();
+});
+
 server.listen(3333);
